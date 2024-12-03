757ffcb4ea7ea2b23bc00d83a474ba929d1fbf1d
JGEVENTS69 <jeson.jgevents@gmail.com>
2024-12-03 13:52:18 +0100

    Maj-Navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,23 +1,13 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { useAuthStore } from '../store/authStore';
-import { BookOpen, Map, User, LogOut, MenuSquare, X, MessageCircleX, Circle, XCircle } from 'lucide-react';
-import { BiCloset } from 'react-icons/bi';
-
-const Layout = ({ children }) => {
-  return (
-    <div className="min-h-screen flex flex-col">
-      <Navigation />
-      <main className="flex-1 pt-16 mt-16">{children}</main>
-    </div>
-  );
-};
+import { BookOpen, Map, User, LogOut, Menu, MenuSquare } from 'lucide-react';
 
 const Navigation = () => {
   const { user, signOut } = useAuthStore();
   const location = useLocation();
+  const navigate = useNavigate(); // Hook pour la navigation
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const [isSidebarOpen, setSidebarOpen] = useState(false);  // Added state for sidebar
   const dropdownRef = useRef(null);
 
   useEffect(() => {
@@ -38,13 +28,22 @@ const Navigation = () => {
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex justify-between items-center h-16 relative">
           {/* Logo */}
-          <Link to="/" className="flex items-center">
+          <button
+            onClick={() => {
+              if (user) {
+                navigate('/map'); // Redirige vers /map si connecté
+              } else {
+                navigate('/'); // Redirige vers / si non connecté
+              }
+            }}
+            className="flex items-center"
+          >
             <img
               src="https://thttmiedctypjsjwdeil.supabase.co/storage/v1/object/public/assets/Logo-Vert.png"
               alt="Logo Bookineo"
               className="h-12 w-30"
             />
-          </Link>
+          </button>
 
           {/* Navigation Links */}
           <div className="flex items-center space-x-4">
@@ -74,77 +73,58 @@ const Navigation = () => {
               /* Dropdown pour utilisateurs connectés */
               <div className="relative" ref={dropdownRef}>
                 <button
-                  onClick={() => setSidebarOpen(!isSidebarOpen)}  // Toggle sidebar on click
+                  onClick={() => setIsDropdownOpen(!isDropdownOpen)}
                   className="flex items-center px-3 py-2 text-sm font-medium text-gray-500 hover:text-primary hover:bg-gray-50 rounded-md transition-colors duration-200"
-                  aria-expanded={isSidebarOpen}
+                  aria-expanded={isDropdownOpen}
                   aria-haspopup="true"
                 >
                   <MenuSquare className="h-8 w-8" />
                   <span className="ml-2 hidden md:inline">Menu</span>
                 </button>
 
-                {/* Sidebar Menu - Menu Latéral */}
-                {isSidebarOpen && (
-                  <div
-                    className="fixed inset-0 bg-gray-800 bg-opacity-50 z-[9998]"
-                    onClick={() => setSidebarOpen(false)}  // Close sidebar when clicking outside
-                  />
-                )}
-
-                <div
-                  className={`fixed right-0 top-0 w-64 h-full bg-white shadow-lg z-[9999] transform transition-all duration-300 ease-in-out ${
-                    isSidebarOpen ? 'translate-x-0' : 'translate-x-full'
-                  }`}
-                >
-                  <div className="flex justify-between items-center px-4 py-4">
-                  <span className="text-2xl text-primary font-semibold">Menu</span>
-                    <button
-                      onClick={() => setSidebarOpen(false)}  // Close sidebar on button click
-                      className="text-[#d8596e]"
-                    >
-                      <XCircle className="h-8 w-8" />
-                    </button>
-                  </div>
-
-                  <div className="py-3 space-y-3">
-                    <Link
-                      to="/map"
-                      className="flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-50 transition-colors duration-200"
-                      onClick={() => setSidebarOpen(false)}
-                    >
-                      <Map className="h-5 w-5 mr-2" />
-                      Carte
-                    </Link>
-                    <Link
-                      to="/profile"
-                      className="flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-50 transition-colors duration-200"
-                      onClick={() => setSidebarOpen(false)}
-                    >
-                      <User className="h-5 w-5 mr-2" />
-                      Profil
-                    </Link>
-                    <Link
-                      to="/my-boxes"
-                      className="flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-50 transition-colors duration-200"
-                      onClick={() => setSidebarOpen(false)}
-                    >
-                      <BookOpen className="h-5 w-5 mr-2" />
-                      Mes boîtes
-                    </Link>
-                    <div className="flex justify-center border-t border-gray-100">
-                      <button
-                        onClick={() => {
-                          signOut();
-                          setSidebarOpen(false);
-                        }}
-                        className="inline-flex items-center mt-8 mb-2 px-4 py-2 text-sm font-medium text-white bg-primary hover:bg-primary/90 rounded-md transition-colors duration-200"
+                {/* Dropdown Menu - uniquement pour utilisateurs connectés */}
+                {isDropdownOpen && (
+                  <div className="absolute right-0 mt-4 w-56 rounded-lg shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 z-[9999] transform origin-top-right transition-all duration-200 ease-out">
+                    <div className="py-3">
+                      <Link
+                        to="/map"
+                        className="flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-50 transition-colors duration-200"
+                        onClick={() => setIsDropdownOpen(false)}
                       >
-                        <LogOut className="h-5 w-5 mr-2" />
-                        Se déconnecter
-                      </button>
+                        <Map className="h-5 w-5 mr-2" />
+                        Carte
+                      </Link>
+                      <Link
+                        to="/profile"
+                        className="flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-50 transition-colors duration-200"
+                        onClick={() => setIsDropdownOpen(false)}
+                      >
+                        <User className="h-5 w-5 mr-2" />
+                        Profil
+                      </Link>
+                      <Link
+                        to="/my-boxes"
+                        className="flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-50 transition-colors duration-200"
+                        onClick={() => setIsDropdownOpen(false)}
+                      >
+                        <BookOpen className="h-5 w-5 mr-2" />
+                        Mes boîtes
+                      </Link>
+                      <div className="flex justify-center border-t border-gray-100">
+                        <button
+                          onClick={() => {
+                            signOut();
+                            setIsDropdownOpen(false);
+                          }}
+                          className="inline-flex items-center mt-8 mb-2 px-4 py-2 text-sm font-medium text-white bg-primary hover:bg-primary/90 rounded-md transition-colors duration-200"
+                        >
+                          <LogOut className="h-5 w-5 mr-2" />
+                          Se déconnecter
+                        </button>
+                      </div>
                     </div>
                   </div>
-                </div>
+                )}
               </div>
             )}
           </div>
